7c483c2ca4de8340b57c5fc72d95b6bea4dfe0ca
Julian Coombes <julian.coombes@googlemail.com>
2018-11-20 12:18:54 +0000

    Extended Permsion testcase coverage

--- pkg/model/permission_test.go
+++ pkg/model/permission_test.go
@@ -28,6 +28,8 @@ func TestPermissionListReturned(t *testing.T) {
 func TestSpecifiedPermissionName(t *testing.T) {
 	perm := GetPermissionFromName("ReadTransactionsDetail")
 	assert.Equal(t, "ReadTransactionsDetail", perm.Permission)
+	perm = GetPermissionFromName("SugarCoatedApple")
+	assert.Equal(t, len(perm.Endpoints), 0)
 }
 
 // feature/refapp_466_add_permissions_to_testcase
@@ -213,4 +215,49 @@ func loadPermissionTestData() (Manifest, error) {
 	}
 	//fmt.Printf(string(pkgutils.DumpJSON(m)))
 	return m, nil
+}
+
+// Permissionset Test cases
+
+func TestGetSetPermissionSetNames(t *testing.T) {
+	p := NewPermissionSet("test", []string{"ReadTransactionsBasic", "ReadTransactionsCredits", "ReadTransactionsDebits"})
+	assert.Equal(t, "test", p.GetName())
+	p.SetName("anothertest")
+	assert.Equal(t, "anothertest", p.GetName())
+}
+
+func TestGetPermissionFromSet(t *testing.T) {
+	p := NewPermissionSet("test", []string{"ReadTransactionsBasic", "ReadTransactionsCredits", "ReadTransactionsDebits"})
+	permission := p.Get("ReadTransactionsDebits")
+	assert.True(t, permission)
+	permission = p.Get("nonexistent")
+	assert.False(t, permission)
+}
+
+func TestRemovePermissionFromSet(t *testing.T) {
+	p := NewPermissionSet("test", []string{"ReadTransactionsBasic", "ReadTransactionsCredits", "ReadTransactionsDebits"})
+	permission := p.Get("ReadTransactionsDebits")
+	assert.True(t, permission)
+	p.Remove("ReadTransactionsDebits")
+	assert.False(t, p.Get("ReadTransactionsDebit"))
+}
+
+func TestPermissionSetSubSet(t *testing.T) {
+	superset := NewPermissionSet("super", []string{"ReadTransactionsBasic", "ReadTransactionsCredits", "ReadTransactionsDebits"})
+	subset := NewPermissionSet("sub", []string{"ReadTransactionsDebits"})
+	issubset := superset.IsSubset(subset)
+	assert.True(t, issubset)
+	subset2 := NewPermissionSet("notsub", []string{"ReadTransactionsDebits_1"})
+	issubset = superset.IsSubset(subset2)
+	assert.False(t, issubset)
+}
+
+func TestPermissionSetUnion(t *testing.T) {
+	set1 := NewPermissionSet("set1", []string{"ReadTransactionsBasic", "ReadTransactionsCredits", "ReadTransactionsDebits"})
+	set2 := NewPermissionSet("set2", []string{"ReadProducts", "ReadOffers", "ReadPartyPSU"})
+	assert.False(t, set1.IsSubset(set2))
+	assert.False(t, set2.IsSubset(set1))
+	union := set1.Union(set2)
+	assert.True(t, union.IsSubset(set1))
+	assert.True(t, union.IsSubset(set2))
 }
