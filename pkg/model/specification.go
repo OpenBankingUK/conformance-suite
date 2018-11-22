b20f2d441bd403796340e3abd3dc6c1f24ed8537
robmckinnon <rob@movingflow>
2018-11-22 12:53:28 +0000

    Add validation of specifications.json config fields
    Add test of loading specifications config.

--- pkg/model/specification.go
+++ pkg/model/specification.go
@@ -8,6 +8,8 @@ import (
 	"os"
 
 	"github.com/sirupsen/logrus"
+
+	validator "gopkg.in/go-playground/validator.v9"
 )
 
 // Represents OB API specification.
@@ -15,41 +17,51 @@ import (
 // URL - URL of confluence specification file.
 // SchemaVersion - URL of OpenAPI/Swagger specification file.
 type specification struct {
-	Identifier    string `json:"identifier,omitempty"`
-	Name          string `json:"name,omitempty"`
-	URL           string `json:"url,omitempty"`
-	Version       string `json:"version,omitempty"`
-	SchemaVersion string `json:"schemaVersion,omitempty"`
+	Identifier    string `json:"identifier,omitempty" validate:"required"`
+	Name          string `json:"name,omitempty" validate:"required"`
+	URL           string `json:"url,omitempty" validate:"required,url"`
+	Version       string `json:"version,omitempty" validate:"required"`
+	SchemaVersion string `json:"schemaVersion,omitempty" validate:"required,url"`
 }
 
 var specifications []specification
 
+// init - load and validate specification data from json file
 func init() {
-	err := loadSpecifications()
+	err := loadDefaultSpecifications()
 	if err != nil {
 		logrus.Error(err)
 		os.Exit(1) // Abort if we can't read the config correctly
 	}
 }
 
-// loadSpecifications - get specification data from json file
-func loadSpecifications() error {
+// loadDefaultSpecifications - load and validate specification data from json file
+func loadDefaultSpecifications() error {
 	file := "./config/specifications.json"
 	if flag.Lookup("test.v") != nil {
 		file = "../../config/specifications.json" // different path when running tests
 	}
 
 	rawjson, err := ioutil.ReadFile(file)
-
-	if err != nil {
-		return err
+	if err == nil {
+		err = loadSpecifications(rawjson)
 	}
+	return err
+}
 
-	if err := json.Unmarshal(rawjson, &specifications); err != nil {
-		return err
+// loadDefaultSpecifications - load and validate specification data from json
+func loadSpecifications(rawjson []byte) error {
+	err := json.Unmarshal(rawjson, &specifications)
+	if err == nil {
+		validate := validator.New()
+		for _, specConfig := range specifications {
+			err = validate.Struct(specConfig)
+			if err != nil {
+				break // sufficient to report validation errors one at a time
+			}
+		}
 	}
-
-	return nil
+	return err
 }
 
 // SpecificationIdentifierFromSchemaVersion - returns specification identifier
--- pkg/model/specification_test.go
+++ pkg/model/specification_test.go
@@ -6,14 +6,60 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func specsFixture(values map[string]string) []byte {
+	schemaVersion, ok := values["schemaVersion"]
+	if !ok {
+		schemaVersion = "https://raw.githubusercontent.com/OpenBankingUK/read-write-api-specs/v3.0.0/dist/account-info-swagger.json"
+	}
+	url, ok := values["url"]
+	if !ok {
+		url = "https://openbanking.atlassian.net/wiki/spaces/DZ/pages/642090641/Account+and+Transaction+API+Specification+-+v3.0"
+	}
+	version, ok := values["version"]
+	if !ok {
+		version = "v3.0"
+	}
+	return []byte(`[
+		  {
+		    "identifier": "account-transaction-v3.0",
+		    "name": "Account and Transaction API Specification",
+		    "url": "` + url + `",
+		    "version": "` + version + `",
+		    "schemaVersion": "` + schemaVersion + `"
+		  }
+		]`)
+}
+
+func assertSpecLoadError(t *testing.T, field string, value string, expected string) {
+	config := specsFixture(map[string]string{field: value})
+	err := loadSpecifications(config)
+	assert.Equal(t, expected, err.Error())
+}
+
+func TestLoadSpecifications_invalid(t *testing.T) {
+	defer loadDefaultSpecifications() // set config back to a valid state for next test
+
+	expected := "Key: 'specification.SchemaVersion' Error:Field validation for 'SchemaVersion' failed on the 'url' tag"
+	assertSpecLoadError(t, "schemaVersion", "invalid-url", expected)
+
+	expected = "Key: 'specification.URL' Error:Field validation for 'URL' failed on the 'url' tag"
+	assertSpecLoadError(t, "url", "invalid-url", expected)
+
+	expected = "Key: 'specification.URL' Error:Field validation for 'URL' failed on the 'required' tag"
+	assertSpecLoadError(t, "url", "", expected)
+
+	expected = "Key: 'specification.Version' Error:Field validation for 'Version' failed on the 'required' tag"
+	assertSpecLoadError(t, "version", "", expected)
+}
+
 func TestSpecificationIdentifierFromSchemaVersion(t *testing.T) {
-	t.Run("returns specification identifier when valid schema version URL", func(t *testing.T) {
+	t.Run("returns specification identifier when given valid schema version URL", func(t *testing.T) {
 		specification, err := SpecificationIdentifierFromSchemaVersion("https://raw.githubusercontent.com/OpenBankingUK/read-write-api-specs/v3.0.0/dist/account-info-swagger.json")
 		assert.Nil(t, err)
 		assert.Equal(t, specification, "account-transaction-v3.0")
 	})
 
-	t.Run("returns error when invalid schema version URL", func(t *testing.T) {
+	t.Run("returns error when given invalid schema version URL", func(t *testing.T) {
 		schemaVersion := "https://example.com/invalid"
 		specification, err := SpecificationIdentifierFromSchemaVersion(schemaVersion)
 		assert.Equal(t, err.Error(), "No specification found for schema version: "+schemaVersion)
