a2bf103bcdb04964e235627f3bf74fc49c0c556a
Glyn Jackson <me@glynjackson.org>
2018-11-28 16:06:06 +0000

    add message on landing

--- web/src/components/Landing.vue
+++ web/src/components/Landing.vue
@@ -8,9 +8,19 @@
     <!--   enterButton="Search" -->
     <!--   size="large" /> -->
     <div class="centered">
-      <router-link
-        to="/wizard"
-        class="go-to-config">Start a validation run</router-link>
+      <b-jumbotron
+        header="Oops!"
+        lead="This release is not intended to be executable, come back soon.">
+        <p>This pre-alpha build introduces some of the concepts to participants before the official release. </p>
+        <p>We are working hard to foster an open and collaborative tool and welcome your feedback to help us develop the best tool.</p>
+       
+        <b-btn
+          variant="primary"
+          target="_blank"
+          href="https://bitbucket.org/openbankingteam/conformance-suite/src/develop/README.md"
+        >More Info</b-btn>
+      </b-jumbotron>
+      <!-- <router-link to="/wizard" class="go-to-config">Start a validation run</router-link> -->
     </div>
   </div>
 </template>
@@ -24,6 +34,6 @@ export default {
     //     this.$router.push('/config', value);
     //   }
     // },
-  },
+  }
 };
 </script>
