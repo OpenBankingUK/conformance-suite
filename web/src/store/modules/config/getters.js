76db318f56bbf8c25063c46e32d0c38b8746f08a
robmckinnon <rob@movingflow>
2018-12-06 16:47:00 +0000

    Add func to parse discovery validation problems
    Return object with JSON path and error message properties for UI use.

--- web/src/store/modules/config/getters.js
+++ web/src/store/modules/config/getters.js
@@ -1,5 +1,35 @@
+
+const lowercaseFirstLetter = string => string.charAt(0).toLowerCase() + string.slice(1);
+
+const parseProblem = (problem) => {
+  if (problem.indexOf('Key') !== -1 && problem.indexOf('Error') !== -1) {
+    const parts = problem.split('Error');
+
+    const path = parts[0]
+      .replace(/^Key: ?'?/, '')
+      .replace(/'? ?$/, '')
+      .replace(/^Model\./, '')
+      .replace('API', 'Api')
+      .split('.')
+      .map(w => lowercaseFirstLetter(w))
+      .join('.');
+
+    const error = parts[1].replace(/^:/, '');
+
+    return {
+      path,
+      error,
+    };
+  }
+  return {
+    path: null,
+    error: problem,
+  };
+};
+
 export default {
   getConfig: state => state.main,
   getDiscoveryModel: state => state.discoveryModel,
   problems: state => state.problems,
+  discoveryProblems: state => state.problems.map(p => parseProblem(p)),
 };
--- web/src/store/modules/config/getters.spec.js
+++ web/src/store/modules/config/getters.spec.js
@@ -1,5 +1,34 @@
 import getters from './getters';
 
+describe('discoveryProblems', () => {
+  let state;
+
+  beforeEach(() => {
+    state = {
+      problems: [
+        `Key: 'Model.DiscoveryModel.DiscoveryItems[0].APISpecification.Name' Error:Field validation for 'Name' failed on the 'required' tag`, // eslint-disable-line
+        'Unexpected token { in JSON at position 108',
+      ],
+    };
+  });
+
+  it('returns object with JSON `path` property for Key/Error problem string', () => {
+    const list = getters.discoveryProblems(state);
+    expect(list[0].path).toEqual('discoveryModel.discoveryItems[0].apiSpecification.name');
+  });
+
+  it('returns object with `error` property for Key/Error problem string', () => {
+    const list = getters.discoveryProblems(state);
+    expect(list[0].error).toEqual(`Field validation for 'Name' failed on the 'required' tag`); // eslint-disable-line
+  });
+
+  it('returns object with `error` property and null `path` for non Key/Error problem string', () => {
+    const list = getters.discoveryProblems(state);
+    expect(list[1].path).toEqual(null); // eslint-disable-line
+    expect(list[1].error).toEqual(`Unexpected token { in JSON at position 108`); // eslint-disable-line
+  });
+});
+
 describe('Config', () => {
   let state;
   const example = '{"a": 1}';
