469b9edc9110614b5333b3727837574e41ac563d
Glyn Jackson <me@glynjackson.org>
2018-12-24 12:38:02 +0000

    removed var version in template

--- web/src/components/Wizard/DiscoveryTemplateCard.vue
+++ web/src/components/Wizard/DiscoveryTemplateCard.vue
@@ -7,39 +7,36 @@
     @click="selectDiscovery()"
   >
     <div class="card-image">
-      <b-card-img
-        :src="imgSrc"
-        :alt="name"
-      />
+      <b-card-img :src="imgSrc" :alt="name"/>
     </div>
     <p class="card-text">{{ text }}</p>
   </b-card>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions } from "vuex";
 
 export default {
-  name: 'DiscoveryTemplateCard',
+  name: "DiscoveryTemplateCard",
   props: {
     discoveryModel: {
       type: Object,
       private: true,
       default() {
         return null;
-      },
+      }
     },
     image: {
       type: String,
       private: true,
       default() {
         return null;
-      },
-    },
+      }
+    }
   },
   computed: {
     title() {
-      return '';
+      return "";
     },
     name() {
       return this.discoveryModel.name;
@@ -49,18 +46,18 @@ export default {
     },
     imgSrc() {
       return this.image;
-    },
+    }
   },
   methods: {
-    ...mapActions('config', [
-      'setDiscoveryModel',
-    ]),
+    ...mapActions("config", ["setDiscoveryModel"]),
     selectDiscovery() {
-      this.setDiscoveryModel(JSON.stringify({ discoveryModel: this.discoveryModel }));
+      this.setDiscoveryModel(
+        JSON.stringify({ discoveryModel: this.discoveryModel })
+      );
       // route to discovery configuration
-      this.$router.push('discovery-config');
-    },
-  },
+      this.$router.push("discovery-config");
+    }
+  }
 };
 </script>
 
@@ -78,71 +75,70 @@ div.card-image {
 }
 .discovery-card > .card-body {
   padding-top: 0.5rem;
-}set
-.card {
-
-    background:#fff;
-    box-shadow:0 20px 50px rgba(0,0,0,.1);
-    border-radius:10px;
-    transition:0.5s;
+}
+set .card {
+  background: #fff;
+  box-shadow: 0 20px 50px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  transition: 0.5s;
 }
 .card:hover {
-    box-shadow:0 30px 70px rgba(0,0,0,.2);
+  box-shadow: 0 30px 70px rgba(0, 0, 0, 0.2);
 }
 .card .box {
-    position:absolute;
-    top:50%;
-    left:0;
-    transform:translateY(-50%);
-    text-align:center;
-    padding:20px;
-    box-sizing:border-box;
-    width:100%;
+  position: absolute;
+  top: 50%;
+  left: 0;
+  transform: translateY(-50%);
+  text-align: center;
+  padding: 20px;
+  box-sizing: border-box;
+  width: 100%;
 }
 .card .box .img {
-    width:120px;
-    height:120px;
-    margin:0 auto;
-    border-radius:50%;
-    overflow:hidden;
+  width: 120px;
+  height: 120px;
+  margin: 0 auto;
+  border-radius: 50%;
+  overflow: hidden;
 }
 .card .box .img img {
-    width:100%;
-    height:100%;
+  width: 100%;
+  height: 100%;
 }
 .card .box h2 {
-    font-size:20px;
-    color:#262626;
-    margin:20px auto;
+  font-size: 20px;
+  color: #262626;
+  margin: 20px auto;
 }
 .card .box h2 span {
-    font-size:14px;
-    background:#e91e63;
-    color:#fff;
-    display:inline-block;
-    padding:4px 10px;
-    border-radius:15px;
+  font-size: 14px;
+  background: #e91e63;
+  color: #fff;
+  display: inline-block;
+  padding: 4px 10px;
+  border-radius: 15px;
 }
 .card .box p {
-    color:#262626;
+  color: #262626;
 }
 .card .box span {
-    display:inline-flex;
+  display: inline-flex;
 }
 .card .box ul {
-    margin:0;
-    padding:0;
+  margin: 0;
+  padding: 0;
 }
 .card .box ul li {
-    list-style:none;
-    float:left;
+  list-style: none;
+  float: left;
 }
 .card .box ul li a {
-    display:block;
-    color:#aaa;
-    margin:0 10px;
-    font-size:20px;
-    transition:0.5s;
-    text-align:center;
+  display: block;
+  color: #aaa;
+  margin: 0 10px;
+  font-size: 20px;
+  transition: 0.5s;
+  text-align: center;
 }
 </style>
