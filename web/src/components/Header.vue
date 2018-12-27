c52cdf3d553b14ea6b6a18ead105b2d72d1954aa
Glyn Jackson <me@glynjackson.org>
2018-12-27 09:39:15 +0000

    experimental styles

---
+++ web/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+<b-navbar>
+      <b-navbar-brand to="/">Functional Conformance Suite</b-navbar-brand>
+    </b-navbar>
+
+
+    
+</template>
+
+
+<style scoped>
+.navbar {
+background: linear-gradient(90deg,#6180C3,#6180C3);
+box-shadow: 0 6px 25px 0 rgba(38,50,56,.2);
+
+
+}
+
+.navbar-brand {
+    color: #ffffff;
+}
+
+</style>
+
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
--- web/src/components/Wizard/ContinueOrStart.vue
+++ web/src/components/Wizard/ContinueOrStart.vue
@@ -1,40 +1,116 @@
 <template>
-  <div class="align-items-start">
-    <h2 class="pt-3 pb-2 mb-3">Continue or Select Disvovery Template</h2>
+  <div class="d-flex flex-row flex-fill">
+    <div class="d-flex align-items-start">
+      <div class="panel w-50" style="height:300px">
+          <div class="panel-heading"><h5>Continue Test</h5></div>
+      </div>
+      <div class="panel w-50" style="height:300px">
 
-    <p>Upload a signed and compatible report to view the results or to rerun.</p>
-    <b-button class="mb-4">Choose file</b-button>
+        <div class="panel-heading"><h5>Continue Test</h5></div>
+        <div class="panel-body">
+          <p>Upload a signed and compatible report to view the results or to rerun.</p>
+          <b-button class="mb-4">Choose file</b-button>
+        </div>
+      </div>
+    </div>
 
-    <hr class="border-bottom w-100">
-    <p>Or start a new test. Choose from a selection of discovery templates to begin.</p>
+    <div class="d-flex align-items-end">
 
-    <b-card-group deck>
-      <DiscoveryTemplateCard
-        v-for="(template, index) in discoveryTemplates"
-        :key="index"
-        :discoveryModel="template.model.discoveryModel"
-        :image="template.image"
-      />
-    </b-card-group>
+      <div class="panel w-100">
+
+          <div class="panel-heading"><h5>Or choose from a selection of discovery templates to begin.</h5></div>
+
+
+          <div class="panel-body">
+              <b-card-group deck>
+        <DiscoveryTemplateCard
+          v-for="(template, index) in discoveryTemplates"
+          :key="index"
+          :discoveryModel="template.model.discoveryModel"
+          :image="template.image"
+        />
+      </b-card-group>
+      
+        </div>
+
+
+          
+      </div>
+
+      
+
+
+      
+
+
+      
+
+
+    </div>
   </div>
+
+  <!-- <div class="align-items-start">
+
+
+
+  <div class="row">
+
+    
+    <div class="col panel">Column</div>
+    <div class="col panel">Column</div>
+
+    <div class="w-100"></div>
+    <div class="col">Column</div>
+    <div class="col">Column</div>
+  </div>
+
+
+  <div class="flex-row">
+      <div class="panel flex-column">
+        <h2>Continue or Select Disvovery Template</h2>
+
+        <p>Upload a signed and compatible report to view the results or to rerun.</p>
+        <b-button class="mb-4">Choose file</b-button>
+      </div>
+
+      <div class="panel flex-column">
+        a
+      </div>
+
+
+
+    </div>  
+
+    <div class="panel w-100">
+      <p>Or start a new test. Choose from a selection of discovery templates to begin.</p>
+
+      <b-card-group deck>
+        <DiscoveryTemplateCard
+          v-for="(template, index) in discoveryTemplates"
+          :key="index"
+          :discoveryModel="template.model.discoveryModel"
+          :image="template.image"
+        />
+      </b-card-group>
+    </div>
+  </div>-->
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-import DiscoveryTemplateCard from './DiscoveryTemplateCard.vue';
+import { mapGetters } from "vuex";
+import DiscoveryTemplateCard from "./DiscoveryTemplateCard.vue";
 
 export default {
-  name: 'ContinueOrStart',
+  name: "ContinueOrStart",
   components: {
-    DiscoveryTemplateCard,
+    DiscoveryTemplateCard
   },
   computed: {
-    ...mapGetters('config', [
-      'discoveryTemplates',
-    ]),
-  },
+    ...mapGetters("config", ["discoveryTemplates"])
+  }
 };
 </script>
 
 <style>
+
 </style>
