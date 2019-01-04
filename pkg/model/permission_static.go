36928ce3c7e443b165818206793ea25059d64ef1
Jose Pinto <bluecor@gmail.com>
2019-01-04 10:57:32 +0000

    removed load and init func

--- pkg/model/permission_static.go
+++ pkg/model/permission_static.go
@@ -1,192 +1,228 @@
 package model
 
-var (
-	// Get /accounts example json response from ozone
-	permissionStaticData = []byte(
-		`[
-			{
-			  "permission": "ReadAccountsBasic",
-			  "endpoints": [
-				"/accounts",
-				"/accounts/{AccountId}"
-			  ],
-			  "default": true
-			},
-			{
-			  "permission": "ReadAccountsDetail",
-			  "endpoints": [
-				"/accounts",
-				"/accounts/{AccountId}"
-			  ]
-			},
-			{
-			  "permission": "ReadBalances",
-			  "endpoints": [
-				"/balances",
-				"/accounts/{AccountId}/balances"
-			  ]
-			},
-			{
-			  "permission": "ReadBeneficiariesBasic",
-			  "endpoints": [
-				"/beneficiaries",
-				"/accounts/{AccountId}/beneficiaries"
-			  ],
-			  "default": true
-			},
-			{
-			  "permission": "ReadBeneficiariesDetail",
-			  "endpoints": [
-				"/beneficiaries",
-				"/accounts/{AccountId}/beneficiaries"
-			  ]
-			},
-			{
-			  "permission": "ReadDirectDebits",
-			  "endpoints": [
-				"/direct-debits",
-				"/accounts/{AccountId}/direct-debits"
-			  ]
-			},
-			{
-			  "permission": "ReadStandingOrdersBasic",
-			  "endpoints": [
-				"/standing-orders",
-				"/accounts/{AccountId}/standing-orders"
-			  ],
-			  "default": true
-			},
-			{
-			  "permission": "ReadStandingOrdersDetail",
-			  "endpoints": [
-				"/standing-orders",
-				"/accounts/{AccountId}/standing-orders"
-			  ]
-			},
-			{
-			  "permission": "ReadTransactionsBasic",
-			  "endpoints": [
-				"/transactions",
-				"/accounts/{AccountId}/transactions",
-				"/accounts/{AccountId}/statements/{StatementId}/transactions"
-			  ],
-			  "required-one-or-more": [
-				"ReadTransactionsCredits",
-				"ReadTransactionsDebits"
-			  ],
-			  "optional": [
-				"ReadPAN"
-			  ],
-			  "default": true
-			},
-			{
-			  "permission": "ReadTransactionsDetail",
-			  "endpoints": [
-				"/transactions",
-				"/accounts/{AccountId}/transactions",
-				"/accounts/{AccountId}/statements/{StatementId}/transactions"
-			  ],
-			  "required-one-or-more": [
-				"ReadTransactionsCredits",
-				"ReadTransactionsDebits"
-			  ],
-			  "optional": [
-				"ReadPAN"
-			  ]
-			},
-			{
-			  "permission": "ReadTransactionsCredits",
-			  "endpoints": [
-				"/transactions",
-				"/accounts/{AccountId}/transactions",
-				"/accounts/{AccountId}/statements/{StatementId}/transactions"
-			  ],
-			  "required-one-or-more": [
-				"ReadTransactionsBasic",
-				"ReadTransactionsDetail"
-			  ],
-			  "optional": [
-				"ReadPAN"
-			  ]
-			},
-			{
-			  "permission": "ReadTransactionsDebits",
-			  "endpoints": [
-				"/transactions",
-				"/accounts/{AccountId}/transactions",
-				"/accounts/{AccountId}/statements/{StatementId}/transactions"
-			  ],
-			  "required-one-or-more": [
-				"ReadTransactionsBasic",
-				"ReadTransactionsDetail"
-			  ],
-			  "optional": [
-				"ReadPAN"
-			  ]
-			},
-			{
-			  "permission": "ReadStatementsBasic",
-			  "endpoints": [
-				"/statements",
-				" /accounts/{AccountId}/statements"
-			  ]
-			},
-			{
-			  "permission": "ReadStatementsDetail",
-			  "endpoints": [
-				"/statements",
-				" /accounts/{AccountId}/statements",
-				"/accounts/{AccountId}/statements/{StatementId}/file"
-			  ]
-			},
-			{
-			  "permission": "ReadProducts",
-			  "endpoints": [
-				"/products",
-				"/accounts/{AccountId}/product"
-			  ]
-			},
-			{
-			  "permission": "ReadOffers",
-			  "endpoints": [
-				"/offers",
-				"/accounts/{AccountId}/offers"
-			  ]
-			},
-			{
-			  "permission": "ReadParty",
-			  "endpoints": [
-				"/accounts/{AccountId}/party"
-			  ]
-			},
-			{
-			  "permission": "ReadPartyPSU",
-			  "endpoints": [
-				"/party"
-			  ]
-			},
-			{
-			  "permission": "ReadScheduledPaymentsBasic",
-			  "endpoints": [
-				"/scheduled-payments",
-				"/accounts/{AccountId}/scheduled-payments"
-			  ],
-			  "default": true
-			},
-			{
-			  "permission": "ReadScheduledPaymentsDetail",
-			  "endpoints": [
-				"/scheduled-payments",
-				"/accounts/{AccountId}/scheduled-payments"
-			  ]
-			},
-			{
-			  "permission": "ReadPAN",
-			  "endpoints": [
-				"",
-				""
-			  ]
-			}
-		  ]
-		  `)
-)
+var permissions = []Permission{
+	{
+		Code: "ReadAccountsBasic",
+		Endpoints: []string{
+			"/accounts",
+			"/accounts/{AccountId}",
+		},
+		Default:           true,
+		RequiredOneOrMore: []Code{},
+		Optional:          []Code{},
+	},
+	{
+		Code: "ReadAccountsDetail",
+		Endpoints: []string{
+			"/accounts",
+			"/accounts/{AccountId}",
+		},
+		Default:           false,
+		RequiredOneOrMore: []Code{},
+		Optional:          []Code{},
+	},
+	{
+		Code: "ReadBalances",
+		Endpoints: []string{
+			"/balances",
+			"/accounts/{AccountId}/balances",
+		},
+		Default:           false,
+		RequiredOneOrMore: []Code{},
+		Optional:          []Code{},
+	},
+	{
+		Code: "ReadBeneficiariesBasic",
+		Endpoints: []string{
+			"/beneficiaries",
+			"/accounts/{AccountId}/beneficiaries",
+		},
+		Default:           true,
+		RequiredOneOrMore: []Code{},
+		Optional:          []Code{},
+	},
+	{
+		Code: "ReadBeneficiariesDetail",
+		Endpoints: []string{
+			"/beneficiaries",
+			"/accounts/{AccountId}/beneficiaries",
+		},
+		Default:           false,
+		RequiredOneOrMore: []Code{},
+		Optional:          []Code{},
+	},
+	{
+		Code: "ReadDirectDebits",
+		Endpoints: []string{
+			"/direct-debits",
+			"/accounts/{AccountId}/direct-debits",
+		},
+		Default:           false,
+		RequiredOneOrMore: []Code{},
+		Optional:          []Code{},
+	},
+	{
+		Code: "ReadStandingOrdersBasic",
+		Endpoints: []string{
+			"/standing-orders",
+			"/accounts/{AccountId}/standing-orders",
+		},
+		Default:           true,
+		RequiredOneOrMore: []Code{},
+		Optional:          []Code{},
+	},
+	{
+		Code: "ReadStandingOrdersDetail",
+		Endpoints: []string{
+			"/standing-orders",
+			"/accounts/{AccountId}/standing-orders",
+		},
+		Default:           false,
+		RequiredOneOrMore: []Code{},
+		Optional:          []Code{},
+	},
+	{
+		Code: "ReadTransactionsBasic",
+		Endpoints: []string{
+			"/transactions",
+			"/accounts/{AccountId}/transactions",
+			"/accounts/{AccountId}/statements/{StatementId}/transactions",
+		},
+		Default: true,
+		RequiredOneOrMore: []Code{
+			"ReadTransactionsCredits",
+			"ReadTransactionsDebits",
+		},
+		Optional: []Code{"ReadPAN"},
+	},
+	{
+		Code: "ReadTransactionsDetail",
+		Endpoints: []string{
+			"/transactions",
+			"/accounts/{AccountId}/transactions",
+			"/accounts/{AccountId}/statements/{StatementId}/transactions",
+		},
+		Default: false,
+		RequiredOneOrMore: []Code{
+			"ReadTransactionsCredits",
+			"ReadTransactionsDebits",
+		},
+		Optional: []Code{"ReadPAN"},
+	},
+	{
+		Code: "ReadTransactionsCredits",
+		Endpoints: []string{
+			"/transactions",
+			"/accounts/{AccountId}/transactions",
+			"/accounts/{AccountId}/statements/{StatementId}/transactions",
+		},
+		Default: false,
+		RequiredOneOrMore: []Code{
+			"ReadTransactionsBasic",
+			"ReadTransactionsDetail",
+		},
+		Optional: []Code{"ReadPAN"},
+	},
+	{
+		Code: "ReadTransactionsDebits",
+		Endpoints: []string{
+			"/transactions",
+			"/accounts/{AccountId}/transactions",
+			"/accounts/{AccountId}/statements/{StatementId}/transactions",
+		},
+		Default: false,
+		RequiredOneOrMore: []Code{
+			"ReadTransactionsBasic",
+			"ReadTransactionsDetail",
+		},
+		Optional: []Code{"ReadPAN"},
+	},
+	{
+		Code: "ReadStatementsBasic",
+		Endpoints: []string{
+			"/statements",
+			"/accounts/{AccountId}/statements",
+		},
+		Default:           false,
+		RequiredOneOrMore: []Code{},
+		Optional:          []Code{},
+	},
+	{
+		Code: "ReadStatementsDetail",
+		Endpoints: []string{
+			"/statements",
+			"/accounts/{AccountId}/statements",
+			"/accounts/{AccountId}/statements/{StatementId}/file",
+		},
+		Default:           false,
+		RequiredOneOrMore: []Code{},
+		Optional:          []Code{},
+	},
+	{
+		Code: "ReadProducts",
+		Endpoints: []string{
+			"/products",
+			"/accounts/{AccountId}/product",
+		},
+		Default:           false,
+		RequiredOneOrMore: []Code{},
+		Optional:          []Code{},
+	},
+	{
+		Code: "ReadOffers",
+		Endpoints: []string{
+			"/offers",
+			"/accounts/{AccountId}/offers",
+		},
+		Default:           false,
+		RequiredOneOrMore: []Code{},
+		Optional:          []Code{},
+	},
+	{
+		Code: "ReadParty",
+		Endpoints: []string{
+			"/accounts/{AccountId}/party",
+		},
+		Default:           false,
+		RequiredOneOrMore: []Code{},
+		Optional:          []Code{},
+	},
+	{
+		Code: "ReadPartyPSU",
+		Endpoints: []string{
+			"/party",
+		},
+		Default:           false,
+		RequiredOneOrMore: []Code{},
+		Optional:          []Code{},
+	},
+	{
+		Code: "ReadScheduledPaymentsBasic",
+		Endpoints: []string{
+			"/scheduled-payments",
+			"/accounts/{AccountId}/scheduled-payments",
+		},
+		Default:           true,
+		RequiredOneOrMore: []Code{},
+		Optional:          []Code{},
+	},
+	{
+		Code: "ReadScheduledPaymentsDetail",
+		Endpoints: []string{
+			"/scheduled-payments",
+			"/accounts/{AccountId}/scheduled-payments",
+		},
+		Default:           false,
+		RequiredOneOrMore: []Code{},
+		Optional:          []Code{},
+	}, {
+		Code: "ReadPAN",
+		Endpoints: []string{
+			"",
+			"",
+		},
+		Default:           false,
+		RequiredOneOrMore: []Code{},
+		Optional:          []Code{},
+	},
+}
