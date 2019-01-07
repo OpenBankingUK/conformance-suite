eaa963258fbb7bb0bd3fc6b361e53f3d8b65e30c
robmckinnon <rob@movingflow>
2019-01-07 14:03:18 +0000

    Change root view to be the wizard start page
    Remove the unneeded landing component.

--- web/src/router.js
+++ web/src/router.js
@@ -12,14 +12,6 @@ const router = new VueRouter({
   routes: [
     {
       path: '/',
-      name: 'Landing',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "landing" */ './components/Landing'),
-    },
-    {
-      path: '/wizard',
       name: 'Wizard',
       redirect: '/wizard/continue-or-start',
       component: () => import(/* webpackChunkName: "wizard" */ './components/Wizard'),
