35b40e866ba399e2feae1e02457900e8c1934478
robmckinnon <rob@movingflow>
2019-01-09 12:00:46 +0000

    Extract ConfigurationFormFile component out of Configuration component
    Use ConfigurationFormFile in Configuration component.

--- web/src/components/Wizard/Configuration.vue
+++ web/src/components/Wizard/Configuration.vue
@@ -8,86 +8,42 @@
         <div class="flex-fill panel-body">
           <div class="d-flex flex-column flex-fill">
             <b-form>
-              <!-- Signing certificates -->
-              <b-form-group
-                id="signing_private_group"
-                :description="getFormGroupDescription('signing_private')"
+              <!--
+              maybe limit file selection to these file types:
+              * .key: application/x-iwork-keynote-sffkey
+              -->
+              <ConfigurationFormFile
+                id="signing_private"
+                field="SigningPrivate"
                 label="Private Signing Certificate (.key):"
-                label-for="signing_private"
-              >
-                <!--
-                maybe limit file selection to these file types:
-                * .key: application/x-iwork-keynote-sffkey
-                -->
-                <b-form-file
-                  id="signing_private"
-                  v-model="files.signing_private"
-                  :state="Boolean(configuration.signing_private)"
-                  placeholder="Choose a file..."
-                  capture
-                  @input="(file) => { onFileChanged(file, 'SigningPrivate') }"
-                />
-              </b-form-group>
-              <b-form-group
-                id="signing_public_group"
-                :description="getFormGroupDescription('signing_public')"
+              />
+              <!--
+              maybe limit file selection to these file types:
+              * .pem: application/x-x509-ca-cert
+              -->
+              <ConfigurationFormFile
+                id="signing_public"
+                field="SigningPublic"
                 label="Public Signing Certificate (.pem):"
-                label-for="signing_public"
-              >
-                <!--
-                maybe limit file selection to these file types:
-                * .pem: application/x-x509-ca-cert
-                -->
-                <b-form-file
-                  id="signing_public"
-                  v-model="files.signing_public"
-                  :state="Boolean(configuration.signing_public)"
-                  placeholder="Choose a file..."
-                  capture
-                  @input="(file) => { onFileChanged(file, 'SigningPublic') }"
-                />
-              </b-form-group>
-
-              <!-- Transport certificates -->
-              <b-form-group
-                id="transport_private_group"
-                :description="getFormGroupDescription('transport_private')"
+              />
+              <!--
+              maybe limit file selection to these file types:
+              * .key: application/x-iwork-keynote-sffkey
+              -->
+              <ConfigurationFormFile
+                id="transport_private"
+                field="TransportPrivate"
                 label="Private Transport Certificate (.key):"
-                label-for="transport_private"
-              >
-                <!--
-                maybe limit file selection to these file types:
-                * .key: application/x-iwork-keynote-sffkey
-                -->
-                <b-form-file
-                  id="transport_private"
-                  v-model="files.transport_private"
-                  :state="Boolean(configuration.transport_private)"
-                  placeholder="Choose a file..."
-                  capture
-                  @input="(file) => { onFileChanged(file, 'TransportPrivate') }"
-                />
-              </b-form-group>
-              <b-form-group
-                id="transport_public_group"
-                :description="getFormGroupDescription('transport_public')"
+              />
+              <!--
+              maybe limit file selection to these file types:
+              * .pem: application/x-x509-ca-cert
+              -->
+              <ConfigurationFormFile
+                id="transport_public"
+                field="TransportPublic"
                 label="Public Transport Certificate (.pem):"
-                label-for="transport_public"
-              >
-                <!--
-                maybe limit file selection to these file types:
-                * .pem: application/x-x509-ca-cert
-                -->
-                <b-form-file
-                  id="transport_public"
-                  v-model="files.transport_public"
-                  :state="Boolean(configuration.transport_public)"
-                  placeholder="Choose a file..."
-                  capture
-                  @input="(file) => { onFileChanged(file, 'TransportPublic') }"
-                />
-                <!-- <div class="mt-3">Selected file: {{ files.transport_public && files.transport_public.name }}</div> -->
-              </b-form-group>
+              />
             </b-form>
             <div v-if="error || configurationErrors.length > 0">
               <h2 class="pt-3 pb-2 mb-3">Errors</h2>
@@ -116,21 +72,17 @@
 import * as _ from 'lodash';
 import { mapGetters, mapActions } from 'vuex';
 
+import ConfigurationFormFile from './ConfigurationFormFile.vue';
 import WizardFooter from './WizardFooter.vue';
 
 export default {
   name: 'Configuration',
   components: {
+    ConfigurationFormFile,
     WizardFooter,
   },
   data() {
     return {
-      files: {
-        signing_private: null,
-        signing_public: null,
-        transport_private: null,
-        transport_public: null,
-      },
       error: null,
     };
   },
@@ -142,81 +94,8 @@ export default {
   },
   methods: {
     ...mapActions('config', [
-      'setConfigurationSigningPrivate',
-      'setConfigurationSigningPublic',
-      'setConfigurationTransportPrivate',
-      'setConfigurationTransportPublic',
       'validateConfiguration',
     ]),
-    /**
-     * Get a description of the file uploaded (when one is selected).
-     * Returns the size and last modification date.
-     */
-    getFormGroupDescription(fileName) {
-      const file = this.files[fileName];
-      const contents = this.configuration[fileName];
-      if (file) {
-        // File (HTML API) contains these fields:
-        // lastModified: 1545301720780
-        // lastModifiedDate: Thu Dec 20 2018 10:28:40 GMT+0000 (Greenwich Mean Time) {}
-        // name: "transport_private.key"
-        // size: 891
-        // type: "application/x-iwork-keynote-sffkey"
-        // webkitRelativePath: ""
-        return [
-          `Size: ${file.size} bytes`,
-          `Last modified: ${file.lastModifiedDate}`,
-        ].join('\n');
-      } else if (contents) {
-        return [
-          `Length: ${contents.length}`,
-        ].join('\n');
-      }
-
-      return '';
-    },
-    /**
-     * readFile turns FileReader API into a Promise-based one,
-     * returning a resolved Promise with the contents of the file
-     * when it has been loaded.
-     */
-    readFile(file) {
-      return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onload = evt => resolve(evt.target.result);
-        reader.onerror = evt => reject(new Error(`reading ${file.name}: ${evt.target.result}`));
-
-        reader.readAsText(file);
-      });
-    },
-    /**
-     * When a file is selected, read its content and set the value in the store.
-     * See: https://stackoverflow.com/questions/45179061/file-input-on-change-in-vue-js
-     */
-    async onFileChanged(file, setterMethodNameSuffix) {
-      // Clear previous error.
-      this.error = null;
-
-      // Compute the method name we need to call in the Vuex store, e.g., could be one of the below:
-      // * setConfigurationSigningPrivate
-      // * setConfigurationSigningPublic
-      const setConfigurationMethodName = `setConfiguration${setterMethodNameSuffix}`;
-      const setConfigurationMethod = this[setConfigurationMethodName];
-
-      if (file) {
-        // If file is set, read the file then set the value in the store.
-        try {
-          const data = await this.readFile(file);
-          setConfigurationMethod(data);
-        } catch (err) {
-          this.error = err.toString();
-        }
-      } else {
-        // If no file selected assume they want to clear out the previous file.
-        const data = '';
-        setConfigurationMethod(data);
-      }
-    },
     /**
      * Validates the configuration.
      */
@@ -255,15 +134,4 @@ export default {
 </script>
 
 <style scoped>
-/* See note on `/deep/` selector: https://vue-loader.vuejs.org/guide/scoped-css.html#deep-selectors */
-
-/* Instead of "Browse" we want "Upload" */
-.b-form-group.form-group .custom-file-input:lang(en) /deep/ .custom-file-label::after {
-  content: "Upload";
-}
-
-/* Ensure line breaks (\n) in the form group description are honoured. */
-.b-form-group.form-group /deep/ .form-text.text-muted {
-  white-space: pre-line;
-}
 </style>
---
+++ web/src/components/Wizard/ConfigurationFormFile.vue
@@ -0,0 +1,152 @@
+<template>
+  <b-form-group
+    :id="`#{id}_group`"
+    :description="getFormGroupDescription(id)"
+    :label="label"
+    :label-for="id"
+  >
+    <b-form-file
+      :id="id"
+      v-model="certificate"
+      :state="isValid"
+      :placeholder="placeholder"
+      capture
+      @input="(file) => { onFileChanged(file, field) }"
+    />
+  </b-form-group>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  name: 'Configuration',
+  props: {
+    id: {
+      type: String,
+      required: true,
+      default: null,
+    },
+    field: {
+      type: String,
+      required: true,
+      default: null,
+    },
+    label: {
+      type: String,
+      required: true,
+      default: null,
+    },
+    placeholder: {
+      type: String,
+      required: true,
+      default: 'Choose a file...',
+    },
+  },
+  data() {
+    return {
+      certificate: null,
+      error: null,
+    };
+  },
+  computed: {
+    ...mapGetters('config', [
+      'configuration',
+    ]),
+    isValid() {
+      return Boolean(this.configuration[this.id]);
+    },
+  },
+  methods: {
+    ...mapActions('config', [
+      'setConfigurationSigningPrivate',
+      'setConfigurationSigningPublic',
+      'setConfigurationTransportPrivate',
+      'setConfigurationTransportPublic',
+    ]),
+    /**
+     * Get a description of the file uploaded (when one is selected).
+     * Returns the size and last modification date.
+     */
+    getFormGroupDescription(fileName) {
+      const file = this.certificate;
+      const contents = this.configuration[fileName];
+      if (file) {
+        // File (HTML API) contains these fields:
+        // lastModified: 1545301720780
+        // lastModifiedDate: Thu Dec 20 2018 10:28:40 GMT+0000 (Greenwich Mean Time) {}
+        // name: "transport_private.key"
+        // size: 891
+        // type: "application/x-iwork-keynote-sffkey"
+        // webkitRelativePath: ""
+        return [
+          `Size: ${file.size} bytes`,
+          `Last modified: ${file.lastModifiedDate}`,
+        ].join('\n');
+      } else if (contents) {
+        return [
+          `Length: ${contents.length}`,
+        ].join('\n');
+      }
+
+      return '';
+    },
+    /**
+     * readFile turns FileReader API into a Promise-based one,
+     * returning a resolved Promise with the contents of the file
+     * when it has been loaded.
+     */
+    readFile(file) {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = evt => resolve(evt.target.result);
+        reader.onerror = evt => reject(new Error(`reading ${file.name}: ${evt.target.result}`));
+
+        reader.readAsText(file);
+      });
+    },
+    /**
+     * When a file is selected, read its content and set the value in the store.
+     * See: https://stackoverflow.com/questions/45179061/file-input-on-change-in-vue-js
+     */
+    async onFileChanged(file, setterMethodNameSuffix) {
+      // Clear previous error.
+      this.error = null;
+
+      // Compute the method name we need to call in the Vuex store, e.g., could be one of the below:
+      // * setConfigurationSigningPrivate
+      // * setConfigurationSigningPublic
+      const setConfigurationMethodName = `setConfiguration${setterMethodNameSuffix}`;
+      const setConfigurationMethod = this[setConfigurationMethodName];
+
+      if (file) {
+        // If file is set, read the file then set the value in the store.
+        try {
+          const data = await this.readFile(file);
+          setConfigurationMethod(data);
+        } catch (err) {
+          this.error = err.toString();
+        }
+      } else {
+        // If no file selected assume they want to clear out the previous file.
+        const data = '';
+        setConfigurationMethod(data);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* See note on `/deep/` selector: https://vue-loader.vuejs.org/guide/scoped-css.html#deep-selectors */
+
+/* Instead of "Browse" we want "Upload" */
+.b-form-group.form-group .custom-file-input:lang(en) /deep/ .custom-file-label::after {
+  content: "Upload";
+}
+
+/* Ensure line breaks (\n) in the form group description are honoured. */
+.b-form-group.form-group /deep/ .form-text.text-muted {
+  white-space: pre-line;
+}
+</style>
