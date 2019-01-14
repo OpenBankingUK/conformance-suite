88a33ea428d5e38106c18dfbd0055f417dfe0256
Jose Pinto <bluecor@gmail.com>
2019-01-14 15:30:41 +0000

    added command lib

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,14 +1,37 @@
 package main
 
 import (
+	"bitbucket.org/openbankingteam/conformance-suite/internal/pkg/version"
 	"fmt"
+	"github.com/spf13/cobra"
 	"os"
 )
 
-const banner = "Functional Conformance Suite CLI"
-
 func main() {
-	fmt.Println(banner)
-	fmt.Println("Nothing here to see.")
-	os.Exit(1)
+	rootCmd := createRootCommand()
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func createRootCommand() *cobra.Command {
+	root := &cobra.Command{
+		Use:   "fcs",
+		Short: "Functional Conformance Suite CLI",
+		Long:  `To use with pipelines and reproducible test runs`,
+	}
+
+	versionCmd := &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number of FCS CLI",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("FCS CLI version: %s\n", version.Version)
+		},
+	}
+
+	root.AddCommand(versionCmd)
+
+	return root
 }
