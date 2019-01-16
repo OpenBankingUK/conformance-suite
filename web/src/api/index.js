8cc3ff34396329649b6e7dc164d18b2fe3fdd68f
robmckinnon <rob@movingflow>
2019-01-16 10:51:26 +0000

    Move config API client calls into separate file
    Move functions into src/api/config.js file.

--- web/src/api/index.js
+++ web/src/api/index.js
@@ -1,34 +1,9 @@
+import config from './config';
 import discovery from './discovery';
 
 const INPUT_PREFIX = '/api';
 
 export default {
-  /**
-   * Call POST /api/config/global
-   * @param {*} configuration Object containing signing_private,
-   * signing_public, transport_private and transport_public fields.
-   */
-  async validateConfiguration(configuration) {
-    const input = `${INPUT_PREFIX}/config/global`;
-    const init = {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json; charset=UTF-8',
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: JSON.stringify(configuration),
-    };
-    const response = await fetch(input, init);
-    const data = await response.json();
-
-    // `fetch` does not throw an error even when status is not 200.
-    // See: https://github.com/whatwg/fetch/issues/18
-    if (response.status !== 201) {
-      throw data;
-    }
-
-    return data;
-  },
   /**
    * Call GET /api/test-cases
    */
@@ -98,5 +73,6 @@ export default {
 
     return data;
   },
+  ...config,
   ...discovery,
 };
