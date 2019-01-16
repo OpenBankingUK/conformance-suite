e1115d5b9e882b1c68ce6650fba4c30822c61cbe
robmckinnon <rob@movingflow>
2019-01-16 14:21:28 +0000

    Move results API client calls into separate file
    Move functions into src/api/results.js file.

--- web/src/api/index.js
+++ web/src/api/index.js
@@ -1,34 +1,11 @@
 import config from './config';
 import discovery from './discovery';
+import results from './results';
 import testcases from './testcases';
 
-const INPUT_PREFIX = '/api';
-
 export default {
-  /**
-   * Call GET /api/report
-   */
-  async computeTestCaseResults() {
-    const input = `${INPUT_PREFIX}/report`;
-    const init = {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json; charset=UTF-8',
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-    };
-    const response = await fetch(input, init);
-    const data = await response.json();
-
-    // `fetch` does not throw an error even when status is not 200.
-    // See: https://github.com/whatwg/fetch/issues/18
-    if (response.status !== 200) {
-      throw data;
-    }
-
-    return data;
-  },
   ...config,
   ...discovery,
+  ...results,
   ...testcases,
 };
