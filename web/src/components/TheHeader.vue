8c870ec44415043b4420888ef104ce3d20b9e087
Conor Hackett <chackett1@gmail.com>
2019-01-21 15:55:56 +0000

    First commit of notifier bell, work in progress

--- web/src/components/TheHeader.vue
+++ web/src/components/TheHeader.vue
@@ -1,12 +1,61 @@
 <template>
   <b-navbar>
     <b-navbar-brand to="/">Conformance Suite</b-navbar-brand>
+    <notification-bell
+      v-b-popover.click.blur.bottomleft.html="notificationText"
+      id="notifyPopover"
+      ref="notify-bell"
+      :size="25"
+      :count="count"
+      :animated="true"
+      title="Notifications"
+      tabindex="0"
+      iconColor="#fff"
+    />
   </b-navbar>
+
 </template>
 
 <script>
+import NotificationBell from 'vue-notification-bell';
+
 export default {
   name: 'TheHeader',
+  components: {
+    NotificationBell,
+  },
+  data() {
+    return {
+      unreadNotifications: 0,
+      notifications: [],
+    };
+  },
+  computed: {
+    count() {
+      return this.unreadNotifications;
+    },
+    notificationText() {
+      if (this.notifications.length === 0) {
+        return 'There are no notifications';
+      }
+
+      let result = '<ul>';
+      for (let i = 0; i < this.notifications.length; i += 1) {
+        result += `<li>${this.notifications[i]}</li>`;
+      }
+      return `${result}</ul>`;
+    },
+  },
+  methods: {
+    pushNotification(message) {
+      this.notifications.push(message);
+      this.unreadNotifications += 1;
+    },
+    shown() {
+      this.unreadNotifications = 0;
+    },
+  },
+
 };
 </script>
 
