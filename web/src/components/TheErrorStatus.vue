014d8c823f7cfac6572dbbd3f2a473abed3a03a4
robmckinnon <rob@movingflow>
2019-01-22 12:25:39 +0000

    Add component to display errors
    Display errors stored in status vuex module.

---
+++ web/src/components/TheErrorStatus.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id='error-status' v-if="hasErrors">
+    <b-alert
+      v-for="(err, index) in errorMessages"
+      :key="index"
+      show
+      variant="danger">{{ err }}</b-alert>
+  </div>
+</template>
+
+<script>
+import { createNamespacedHelpers } from 'vuex';
+
+const { mapGetters } = createNamespacedHelpers('status');
+
+export default {
+  name: 'TheErrorStatus',
+  computed: {
+    ...mapGetters([
+      'errorMessages',
+      'hasErrors',
+    ]),
+  },
+};
+</script>
+
+<style scoped>
+</style>
