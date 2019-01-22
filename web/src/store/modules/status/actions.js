212381d4ca071c52ad7c7e4fb45351fe66d11bdd
robmckinnon <rob@movingflow>
2019-01-22 15:10:08 +0000

    Add clearErrors action to status store
    Add tests for setErrors and clearErrors.

--- web/src/store/modules/status/actions.js
+++ web/src/store/modules/status/actions.js
@@ -1,7 +1,14 @@
 import * as types from './mutation-types';
 
 export default {
+  clearErrors({ commit, state }) {
+    if (state.errors.length > 0) {
+      commit(types.SET_ERRORS, []);
+    }
+  },
   setErrors({ commit }, errors) {
-    commit(types.SET_ERRORS, errors);
+    if (errors) {
+      commit(types.SET_ERRORS, errors);
+    }
   },
 };
