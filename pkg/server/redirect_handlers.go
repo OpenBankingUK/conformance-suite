51da765f747f14232eb1f8812b946079caad123d
Conor Hackett <chackett1@gmail.com>
2019-02-04 20:58:46 +0000

    Implement c_hash validation

--- pkg/server/redirect_handlers.go
+++ pkg/server/redirect_handlers.go
@@ -1,7 +1,11 @@
 package server
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
+	"github.com/dgrijalva/jwt-go"
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo"
 	"github.com/sirupsen/logrus"
@@ -15,14 +19,34 @@ type RedirectFragment struct {
 }
 
 type RedirectQuery struct {
-	Code  string `json:"code" form:"code" query:"code"`
-	State string `json:"state" form:"state" query:"state"`
+	Code    string `json:"code" form:"code" query:"code"`
+	Scope   string `json:"scope" form:"scope" query:"scope"`
+	IDToken string `json:"id_token" form:"id_token" query:"id_token"`
+	State   string `json:"state" form:"state" query:"state"`
 }
 
 type RedirectError struct {
 	ErrorDescription string `json:"error_description" form:"error_description" query:"error_description"`
 	Error            string `json:"error" form:"error" query:"error"`
 	State            string `json:"state" form:"state" query:"state"`
+}
+
+// AuthClaim represents an in coming JWT from third part ASPSP as part of authentication/consent
+// process during `Hybrid Flow Authentication`
+// https://openid.net/specs/openid-connect-core-1_0.html#HybridFlowAuth
+type AuthClaim struct {
+	jwt.StandardClaims
+	AuditTrackingID     string `json:"auditTrackingId"`
+	TokenName           string `json:"tokenName"`
+	Nonce               string `json:"nonce"`
+	Acr                 string `json:"acr"`
+	CHash               string `json:"c_hash"`
+	OpenBankingIntentID string `json:"openbanking_intent_id"`
+	SHash               string `json:"s_hash"`
+	Azp                 string `json:"azp"`
+	AuthTime            int    `json:"auth_time"`
+	Realm               string `json:"realm"`
+	TokenType           string `json:"tokenType"`
 }
 
 type redirectHandlers struct {
@@ -36,7 +60,21 @@ func (h *redirectHandlers) postFragmentOKHandler(c echo.Context) error {
 		return err
 	}
 
-	return c.JSON(http.StatusOK, fragment)
+	claim := &AuthClaim{}
+
+	// If not providing Keyfunc (3rd param), don't check for error here
+	// as it will always be error("no Keyfunc was provided")
+	t, _ := jwt.ParseWithClaims(fragment.IDToken, claim, nil)
+
+	if !t.Valid {
+		logrus.Warn("token not valid")
+	}
+
+	cHash := h.calculateCHash(fragment.Code, true)
+	if cHash == claim.CHash {
+		return c.JSON(http.StatusOK, fragment)
+	}
+	return c.JSON(http.StatusBadRequest, fragment)
 }
 
 // postQueryOKHandler - POST /redirect/query/ok
@@ -46,7 +84,21 @@ func (h *redirectHandlers) postQueryOKHandler(c echo.Context) error {
 		return err
 	}
 
-	return c.JSON(http.StatusOK, query)
+	claim := &AuthClaim{}
+
+	// If not providing Keyfunc (3rd param), don't check for error here
+	// as it will always be error("no Keyfunc was provided")
+	t, _ := jwt.ParseWithClaims(query.IDToken, claim, nil)
+
+	if !t.Valid {
+		logrus.Warn("token not valid")
+	}
+
+	cHash := h.calculateCHash(query.Code, true)
+	if cHash == claim.CHash {
+		return c.JSON(http.StatusOK, query)
+	}
+	return c.JSON(http.StatusBadRequest, query)
 }
 
 // postErrorHandler - POST /api/redirect/error
@@ -57,4 +109,19 @@ func (h *redirectHandlers) postErrorHandler(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, redirectError)
+}
+
+// calculateCHash calculates the code hash (c_hash) value
+// as described in section 3.3.2.11 (ID Token) https://openid.net/specs/openid-connect-core-1_0.html#HybridIDToken
+// There is an option to trim the `==` from the end of the base64url encoded string, by setting `trim=true`
+func (h *redirectHandlers) calculateCHash(code string, trim bool) string {
+	digest := sha256.Sum256([]byte(code))
+	left := digest[0 : len(digest)/2]
+	b64 := base64.URLEncoding.EncodeToString(left)
+
+	if trim {
+		return strings.Trim(b64, "=")
+	}
+
+	return b64
 }
--- pkg/server/redirect_handlers_test.go
+++ pkg/server/redirect_handlers_test.go
@@ -9,6 +9,14 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// TestTableItem holds variables for each test run
+type TestTableItem struct {
+	label              string
+	endpoint           string
+	httpStatusExpected int
+	bodyExpected       string
+}
+
 func TestRedirectHandlersFragmentOK(t *testing.T) {
 	require := require.New(t)
 
@@ -18,30 +26,76 @@ func TestRedirectHandlersFragmentOK(t *testing.T) {
 	}()
 	require.NotNil(server)
 
-	bodyExpected := `
+	// Valid code and c_hash combination
+	testItemOK := TestTableItem{
+		label:              "fragment ok",
+		endpoint:           `/api/redirect/fragment/ok`,
+		httpStatusExpected: http.StatusOK,
+		bodyExpected: `
 {
     "code": "a052c795-742d-415a-843f-8a4939d740d1",
     "scope": "openid accounts",
     "id_token": "eyJ0eXAiOiJKV1QiLCJraWQiOiJGb2w3SXBkS2VMWm16S3RDRWdpMUxEaFNJek09IiwiYWxnIjoiRVMyNTYifQ.eyJzdWIiOiJtYmFuYSIsImF1ZGl0VHJhY2tpbmdJZCI6IjY5YzZkZmUzLWM4MDEtNGRkMi05Mjc1LTRjNWVhNzdjZWY1NS0xMDMzMDgyIiwiaXNzIjoiaHR0cHM6Ly9tYXRscy5hcy5hc3BzcC5vYi5mb3JnZXJvY2suZmluYW5jaWFsL29hdXRoMi9vcGVuYmFua2luZyIsInRva2VuTmFtZSI6ImlkX3Rva2VuIiwibm9uY2UiOiI1YTZiMGQ3ODMyYTlmYjRmODBmMTE3MGEiLCJhY3IiOiJ1cm46b3BlbmJhbmtpbmc6cHNkMjpzY2EiLCJhdWQiOiI1NGY2NDMwOS00MzNkLTQ2MTAtOTVkMi02M2QyZjUyNTM0MTIiLCJjX2hhc2giOiIxbGt1SEFuaVJDZlZNS2xEc0pxTTNBIiwib3BlbmJhbmtpbmdfaW50ZW50X2lkIjoiQTY5MDA3Nzc1LTcwZGQtNGIyMi1iZmM1LTlkNTI0YTkxZjk4MCIsInNfaGFzaCI6ImZ0OWRrQTdTWXdlb2hlZXpjOGFHeEEiLCJhenAiOiI1NGY2NDMwOS00MzNkLTQ2MTAtOTVkMi02M2QyZjUyNTM0MTIiLCJhdXRoX3RpbWUiOjE1Mzk5NDM3NzUsInJlYWxtIjoiL29wZW5iYW5raW5nIiwiZXhwIjoxNTQwMDMwMTgxLCJ0b2tlblR5cGUiOiJKV1RUb2tlbiIsImlhdCI6MTUzOTk0Mzc4MX0.8bm69KPVQIuvcTlC-p0FGcplTV1LnmtacHybV2PTb2uEgMgrL3JNA0jpT2OYO73r3zPC41mNQlMDvVOUn78osQ",
     "state": "5a6b0d7832a9fb4f80f1170a"
 }
-	`
-	// read the file we expect to be served.
-	code, body, headers := request(
-		http.MethodPost,
-		`/api/redirect/fragment/ok`,
-		strings.NewReader(bodyExpected),
-		server,
-	)
+	`,
+	}
 
-	// do assertions.
-	require.Equal(http.StatusOK, code)
-	require.Len(headers, 2)
-	require.Equal("application/json; charset=UTF-8", headers["Content-Type"][0])
-	require.NotNil(body)
+	// c_hash will not match calculated c_hash due to invalid `code`
+	testItemInvalidCode := TestTableItem{
+		label:              "fragment invalid code",
+		endpoint:           "/api/redirect/fragment/ok",
+		httpStatusExpected: http.StatusBadRequest,
+		bodyExpected: `
+{
+    "code": "---invalid code---",
+    "scope": "openid accounts",
+    "id_token": "eyJ0eXAiOiJKV1QiLCJraWQiOiJGb2w3SXBkS2VMWm16S3RDRWdpMUxEaFNJek09IiwiYWxnIjoiRVMyNTYifQ.eyJzdWIiOiJtYmFuYSIsImF1ZGl0VHJhY2tpbmdJZCI6IjY5YzZkZmUzLWM4MDEtNGRkMi05Mjc1LTRjNWVhNzdjZWY1NS0xMDMzMDgyIiwiaXNzIjoiaHR0cHM6Ly9tYXRscy5hcy5hc3BzcC5vYi5mb3JnZXJvY2suZmluYW5jaWFsL29hdXRoMi9vcGVuYmFua2luZyIsInRva2VuTmFtZSI6ImlkX3Rva2VuIiwibm9uY2UiOiI1YTZiMGQ3ODMyYTlmYjRmODBmMTE3MGEiLCJhY3IiOiJ1cm46b3BlbmJhbmtpbmc6cHNkMjpzY2EiLCJhdWQiOiI1NGY2NDMwOS00MzNkLTQ2MTAtOTVkMi02M2QyZjUyNTM0MTIiLCJjX2hhc2giOiIxbGt1SEFuaVJDZlZNS2xEc0pxTTNBIiwib3BlbmJhbmtpbmdfaW50ZW50X2lkIjoiQTY5MDA3Nzc1LTcwZGQtNGIyMi1iZmM1LTlkNTI0YTkxZjk4MCIsInNfaGFzaCI6ImZ0OWRrQTdTWXdlb2hlZXpjOGFHeEEiLCJhenAiOiI1NGY2NDMwOS00MzNkLTQ2MTAtOTVkMi02M2QyZjUyNTM0MTIiLCJhdXRoX3RpbWUiOjE1Mzk5NDM3NzUsInJlYWxtIjoiL29wZW5iYW5raW5nIiwiZXhwIjoxNTQwMDMwMTgxLCJ0b2tlblR5cGUiOiJKV1RUb2tlbiIsImlhdCI6MTUzOTk0Mzc4MX0.8bm69KPVQIuvcTlC-p0FGcplTV1LnmtacHybV2PTb2uEgMgrL3JNA0jpT2OYO73r3zPC41mNQlMDvVOUn78osQ",
+    "state": "5a6b0d7832a9fb4f80f1170a"
+}
+	`,
+	}
 
-	bodyActual := body.String()
-	require.JSONEq(bodyExpected, bodyActual)
+	// Note c_hash is manipulated (invalid) in JWT, meaning invalid signature as a result
+	// if signature validation is implemented this test shall fail.
+	testItemInvalidCHash := TestTableItem{
+		label:              "fragment invalid c_hash",
+		endpoint:           "/api/redirect/fragment/ok",
+		httpStatusExpected: http.StatusBadRequest,
+		bodyExpected: `
+{
+    "code": "a052c795-742d-415a-843f-8a4939d740d1",
+    "scope": "openid accounts",
+    "id_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtYmFuYSIsImF1ZGl0VHJhY2tpbmdJZCI6IjY5YzZkZmUzLWM4MDEtNGRkMi05Mjc1LTRjNWVhNzdjZWY1NS0xMDMzMDgyIiwiaXNzIjoiaHR0cHM6Ly9tYXRscy5hcy5hc3BzcC5vYi5mb3JnZXJvY2suZmluYW5jaWFsL29hdXRoMi9vcGVuYmFua2luZyIsInRva2VuTmFtZSI6ImlkX3Rva2VuIiwibm9uY2UiOiI1YTZiMGQ3ODMyYTlmYjRmODBmMTE3MGEiLCJhY3IiOiJ1cm46b3BlbmJhbmtpbmc6cHNkMjpzY2EiLCJhdWQiOiI1NGY2NDMwOS00MzNkLTQ2MTAtOTVkMi02M2QyZjUyNTM0MTIiLCJjX2hhc2giOiJiYWQtY19oYXNoIiwib3BlbmJhbmtpbmdfaW50ZW50X2lkIjoiQTY5MDA3Nzc1LTcwZGQtNGIyMi1iZmM1LTlkNTI0YTkxZjk4MCIsInNfaGFzaCI6ImZ0OWRrQTdTWXdlb2hlZXpjOGFHeEEiLCJhenAiOiI1NGY2NDMwOS00MzNkLTQ2MTAtOTVkMi02M2QyZjUyNTM0MTIiLCJhdXRoX3RpbWUiOjE1Mzk5NDM3NzUsInJlYWxtIjoiL29wZW5iYW5raW5nIiwiZXhwIjoxNTQ5MzEzODk3LCJ0b2tlblR5cGUiOiJKV1RUb2tlbiIsImlhdCI6MTUzOTk0Mzc4MSwianRpIjoiNTY5NDgxMDEtYjY0NS00NjFkLThhNzUtYTgzNmZiMGVmODAzIn0.GGWSIW6SI2XXZkfIZq6QJ5O-j6BHZbqOGvCWxOaNypU",
+    "state": "5a6b0d7832a9fb4f80f1170a"
+}
+	`,
+	}
+
+	ttData := []TestTableItem{
+		testItemOK,
+		testItemInvalidCode,
+		testItemInvalidCHash,
+	}
+
+	for _, ttItem := range ttData {
+		// read the file we expect to be served.
+		code, body, headers := request(
+			http.MethodPost,
+			ttItem.endpoint,
+			strings.NewReader(ttItem.bodyExpected),
+			server,
+		)
+
+		// do assertions.
+		require.Equal(ttItem.httpStatusExpected, code, ttItem.label)
+		require.Len(headers, 2, ttItem.label)
+		require.Equal("application/json; charset=UTF-8", headers["Content-Type"][0], ttItem.label)
+		require.NotNil(body, ttItem.label)
+
+		bodyActual := body.String()
+		require.JSONEq(ttItem.bodyExpected, bodyActual, ttItem.label)
+	}
 }
 
 func TestRedirectHandlersQueryOK(t *testing.T) {
@@ -53,28 +107,75 @@ func TestRedirectHandlersQueryOK(t *testing.T) {
 	}()
 	require.NotNil(server)
 
-	bodyExpected := `
+	// valid code and c_hash combination
+	testItemOK := TestTableItem{
+		label:              "query ok",
+		endpoint:           `/api/redirect/query/ok`,
+		httpStatusExpected: http.StatusOK,
+		bodyExpected: `
 {
     "code": "a052c795-742d-415a-843f-8a4939d740d1",
+    "scope": "openid accounts",
+    "id_token": "eyJ0eXAiOiJKV1QiLCJraWQiOiJGb2w3SXBkS2VMWm16S3RDRWdpMUxEaFNJek09IiwiYWxnIjoiRVMyNTYifQ.eyJzdWIiOiJtYmFuYSIsImF1ZGl0VHJhY2tpbmdJZCI6IjY5YzZkZmUzLWM4MDEtNGRkMi05Mjc1LTRjNWVhNzdjZWY1NS0xMDMzMDgyIiwiaXNzIjoiaHR0cHM6Ly9tYXRscy5hcy5hc3BzcC5vYi5mb3JnZXJvY2suZmluYW5jaWFsL29hdXRoMi9vcGVuYmFua2luZyIsInRva2VuTmFtZSI6ImlkX3Rva2VuIiwibm9uY2UiOiI1YTZiMGQ3ODMyYTlmYjRmODBmMTE3MGEiLCJhY3IiOiJ1cm46b3BlbmJhbmtpbmc6cHNkMjpzY2EiLCJhdWQiOiI1NGY2NDMwOS00MzNkLTQ2MTAtOTVkMi02M2QyZjUyNTM0MTIiLCJjX2hhc2giOiIxbGt1SEFuaVJDZlZNS2xEc0pxTTNBIiwib3BlbmJhbmtpbmdfaW50ZW50X2lkIjoiQTY5MDA3Nzc1LTcwZGQtNGIyMi1iZmM1LTlkNTI0YTkxZjk4MCIsInNfaGFzaCI6ImZ0OWRrQTdTWXdlb2hlZXpjOGFHeEEiLCJhenAiOiI1NGY2NDMwOS00MzNkLTQ2MTAtOTVkMi02M2QyZjUyNTM0MTIiLCJhdXRoX3RpbWUiOjE1Mzk5NDM3NzUsInJlYWxtIjoiL29wZW5iYW5raW5nIiwiZXhwIjoxNTQwMDMwMTgxLCJ0b2tlblR5cGUiOiJKV1RUb2tlbiIsImlhdCI6MTUzOTk0Mzc4MX0.8bm69KPVQIuvcTlC-p0FGcplTV1LnmtacHybV2PTb2uEgMgrL3JNA0jpT2OYO73r3zPC41mNQlMDvVOUn78osQ",
     "state": "5a6b0d7832a9fb4f80f1170a"
 }
-	`
-	// read the file we expect to be served.
-	code, body, headers := request(
-		http.MethodPost,
-		`/api/redirect/query/ok`,
-		strings.NewReader(bodyExpected),
-		server,
-	)
+	`,
+	}
 
-	// do assertions.
-	require.Equal(http.StatusOK, code)
-	require.Len(headers, 2)
-	require.Equal("application/json; charset=UTF-8", headers["Content-Type"][0])
-	require.NotNil(body)
+	// invalid value for `code`
+	testItemInvalidCode := TestTableItem{
+		label:              "query invalid code",
+		endpoint:           `/api/redirect/query/ok`,
+		httpStatusExpected: http.StatusBadRequest,
+		bodyExpected: `
+{
+    "code": "invalid-code",
+    "scope": "openid accounts",
+    "id_token": "eyJ0eXAiOiJKV1QiLCJraWQiOiJGb2w3SXBkS2VMWm16S3RDRWdpMUxEaFNJek09IiwiYWxnIjoiRVMyNTYifQ.eyJzdWIiOiJtYmFuYSIsImF1ZGl0VHJhY2tpbmdJZCI6IjY5YzZkZmUzLWM4MDEtNGRkMi05Mjc1LTRjNWVhNzdjZWY1NS0xMDMzMDgyIiwiaXNzIjoiaHR0cHM6Ly9tYXRscy5hcy5hc3BzcC5vYi5mb3JnZXJvY2suZmluYW5jaWFsL29hdXRoMi9vcGVuYmFua2luZyIsInRva2VuTmFtZSI6ImlkX3Rva2VuIiwibm9uY2UiOiI1YTZiMGQ3ODMyYTlmYjRmODBmMTE3MGEiLCJhY3IiOiJ1cm46b3BlbmJhbmtpbmc6cHNkMjpzY2EiLCJhdWQiOiI1NGY2NDMwOS00MzNkLTQ2MTAtOTVkMi02M2QyZjUyNTM0MTIiLCJjX2hhc2giOiIxbGt1SEFuaVJDZlZNS2xEc0pxTTNBIiwib3BlbmJhbmtpbmdfaW50ZW50X2lkIjoiQTY5MDA3Nzc1LTcwZGQtNGIyMi1iZmM1LTlkNTI0YTkxZjk4MCIsInNfaGFzaCI6ImZ0OWRrQTdTWXdlb2hlZXpjOGFHeEEiLCJhenAiOiI1NGY2NDMwOS00MzNkLTQ2MTAtOTVkMi02M2QyZjUyNTM0MTIiLCJhdXRoX3RpbWUiOjE1Mzk5NDM3NzUsInJlYWxtIjoiL29wZW5iYW5raW5nIiwiZXhwIjoxNTQwMDMwMTgxLCJ0b2tlblR5cGUiOiJKV1RUb2tlbiIsImlhdCI6MTUzOTk0Mzc4MX0.8bm69KPVQIuvcTlC-p0FGcplTV1LnmtacHybV2PTb2uEgMgrL3JNA0jpT2OYO73r3zPC41mNQlMDvVOUn78osQ",
+    "state": "5a6b0d7832a9fb4f80f1170a"
+}
+	`,
+	}
 
-	bodyActual := body.String()
-	require.JSONEq(bodyExpected, bodyActual)
+	// invalid value for `c_hash` inside the `id_token` field
+	testItemInvalidCHash := TestTableItem{
+		label:              "query invalid c_hash",
+		endpoint:           `/api/redirect/query/ok`,
+		httpStatusExpected: http.StatusBadRequest,
+		bodyExpected: `
+{
+    "code": "a052c795-742d-415a-843f-8a4939d740d1",
+    "scope": "openid accounts",
+    "id_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtYmFuYSIsImF1ZGl0VHJhY2tpbmdJZCI6IjY5YzZkZmUzLWM4MDEtNGRkMi05Mjc1LTRjNWVhNzdjZWY1NS0xMDMzMDgyIiwiaXNzIjoiaHR0cHM6Ly9tYXRscy5hcy5hc3BzcC5vYi5mb3JnZXJvY2suZmluYW5jaWFsL29hdXRoMi9vcGVuYmFua2luZyIsInRva2VuTmFtZSI6ImlkX3Rva2VuIiwibm9uY2UiOiI1YTZiMGQ3ODMyYTlmYjRmODBmMTE3MGEiLCJhY3IiOiJ1cm46b3BlbmJhbmtpbmc6cHNkMjpzY2EiLCJhdWQiOiI1NGY2NDMwOS00MzNkLTQ2MTAtOTVkMi02M2QyZjUyNTM0MTIiLCJjX2hhc2giOiJiYWQtY19oYXNoIiwib3BlbmJhbmtpbmdfaW50ZW50X2lkIjoiQTY5MDA3Nzc1LTcwZGQtNGIyMi1iZmM1LTlkNTI0YTkxZjk4MCIsInNfaGFzaCI6ImZ0OWRrQTdTWXdlb2hlZXpjOGFHeEEiLCJhenAiOiI1NGY2NDMwOS00MzNkLTQ2MTAtOTVkMi02M2QyZjUyNTM0MTIiLCJhdXRoX3RpbWUiOjE1Mzk5NDM3NzUsInJlYWxtIjoiL29wZW5iYW5raW5nIiwiZXhwIjoxNTQ5MzEzODk3LCJ0b2tlblR5cGUiOiJKV1RUb2tlbiIsImlhdCI6MTUzOTk0Mzc4MSwianRpIjoiNTY5NDgxMDEtYjY0NS00NjFkLThhNzUtYTgzNmZiMGVmODAzIn0.GGWSIW6SI2XXZkfIZq6QJ5O-j6BHZbqOGvCWxOaNypU",
+    "state": "5a6b0d7832a9fb4f80f1170a"
+}
+	`,
+	}
+
+	ttData := []TestTableItem{
+		testItemOK,
+		testItemInvalidCode,
+		testItemInvalidCHash,
+	}
+
+	for _, ttItem := range ttData {
+		// read the file we expect to be served.
+		code, body, headers := request(
+			http.MethodPost,
+			ttItem.endpoint,
+			strings.NewReader(ttItem.bodyExpected),
+			server,
+		)
+
+		// do assertions.
+		require.Equal(ttItem.httpStatusExpected, code, ttItem.label)
+		require.Len(headers, 2, ttItem.label)
+		require.Equal("application/json; charset=UTF-8", headers["Content-Type"][0], ttItem.label)
+		require.NotNil(body, ttItem.label)
+
+		bodyActual := body.String()
+		require.JSONEq(ttItem.bodyExpected, bodyActual, ttItem.label)
+	}
 }
 
 func TestRedirectHandlersError(t *testing.T) {
