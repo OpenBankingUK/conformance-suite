2371020a76d6cc8624b3b78c7be99dc6b20a7761
robmckinnon <rob@movingflow>
2019-02-04 16:18:44 +0000

    Set showLoading true when waiting for API calls
    Wrap the get/post functions in apiUtil.js to call given setShowLoading function.

--- web/src/api/apiUtil.js
+++ web/src/api/apiUtil.js
@@ -1,25 +1,49 @@
 export default {
 
   // Async call to get API endpoint, returns promise.
-  get(path) {
-    return fetch(path, {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json; charset=UTF-8',
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-    });
+  async get(path, setShowLoading) {
+    if (setShowLoading) {
+      setShowLoading(true);
+    }
+    try {
+      const response = await fetch(path, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json; charset=UTF-8',
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      });
+      return response;
+    } catch (e) {
+      throw e;
+    } finally {
+      if (setShowLoading) {
+        setShowLoading(false);
+      }
+    }
   },
 
   // Async call to post API endpoint, returns promise.
-  post(path, obj) {
-    return fetch(path, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json; charset=UTF-8',
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: obj ? JSON.stringify(obj) : null,
-    });
+  async post(path, obj, setShowLoading) {
+    if (setShowLoading) {
+      setShowLoading(true);
+    }
+    try {
+      const response = await fetch(path, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json; charset=UTF-8',
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: obj ? JSON.stringify(obj) : null,
+      });
+      return response;
+    } catch (e) {
+      throw e;
+    } finally {
+      if (setShowLoading) {
+        setShowLoading(false);
+      }
+    }
   },
 };
