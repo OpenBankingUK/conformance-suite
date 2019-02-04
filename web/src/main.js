8547fa479f0cc801ef0c9183ef8872500f476020
robmckinnon <rob@movingflow>
2019-02-04 06:33:37 +0000

    Remove template setting from main.js
    Move template loading to separate file.
    
    Invoke template loading at store initiation.

--- web/src/main.js
+++ web/src/main.js
@@ -16,21 +16,8 @@ Vue.use(BootstrapVue);
 // Don't warn about using the dev version of Vue in development.
 Vue.config.productionTip = process.env.NODE_ENV === 'production';
 
-// Use webpack require.context to import templates and images.
-// See: https://webpack.js.org/guides/dependency-management/#require-context
-//      https://vuejs.org/v2/guide/components-registration.html#Automatic-Global-Registration-of-Base-Components
-const requireTemplates = require.context('../../pkg/discovery/templates/', false, /.+\.json$/);
-const discoveryTemplates = requireTemplates.keys().map(file => requireTemplates(file));
-
-const requireImages = require.context('./assets/images/', false, /.+\.png$/);
-const discoveryImages = {};
-requireImages.keys().forEach(file => discoveryImages[file] = requireImages(file)); // eslint-disable-line
-
 new Vue({
   router,
   store,
   render: h => h(App),
 }).$mount('#app');
-
-// Store templates and images.
-store.dispatch('config/setDiscoveryTemplates', { discoveryTemplates, discoveryImages }, { root: true });
