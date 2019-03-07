405e873d1aaa64faf76a4427667edd2ba5a2dc53
robmckinnon <rob@movingflow>
2019-03-07 12:48:41 +0000

    Load keys/secrets from ENV VAR for e2e UI tests
    Create a headless config JSON template for Ozone, populate with ENV VAR.

--- web/tests/e2e/specs/headless-consent.js
+++ web/tests/e2e/specs/headless-consent.js
@@ -1,35 +1,25 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-const exampleConfig = {
-  signing_private: '',
-  signing_public: '',
-  transport_private: '',
-  transport_public: '',
-  client_id: '',
-  client_secret: '',
-  token_endpoint: 'https://modelobank2018.o3bank.co.uk:4201/token',
-  token_endpoint_auth_method: 'client_secret_basic',
-  authorization_endpoint: 'https://modelobankauth2018.o3bank.co.uk:4101/auth',
-  resource_base_url: 'https://modelobank2018.o3bank.co.uk:4501',
-  x_fapi_financial_id: '0015800001041RHAAY',
-  issuer: 'https://modelobankauth2018.o3bank.co.uk:4101',
-  redirect_url: 'https://0.0.0.0:8443/conformancesuite/callback',
-};
-
 describe('Headless consent model bank test case run', () => {
   const discoveryTemplate = '#ob-v3-1-ozone-headless';
   const nextButton = '#next';
   const configJsonTab = '#json-view___BV_tab_button__';
 
+  // Note: We can't use async/await with Cypress then() func, as it does not
+  // return a Promise.
+  // See: https://docs.cypress.io/guides/core-concepts/variables-and-aliases.html#Closures
   it('Gets results', () => {
     cy.visit('https://localhost:8443');
     cy.get(discoveryTemplate).click();
     cy.get(nextButton).click();
     cy.get(configJsonTab).click();
-    cy.window().then((win) => {
-      const editor = win.ace.edit('configuration-editor');
-      editor.getSession().setValue(JSON.stringify(exampleConfig, null, 2));
+    cy.configFixture('ozone-headless-config.json').then((config) => {
+      cy.window().then((win) => {
+        const editor = win.ace.edit('configuration-editor');
+        editor.getSession().setValue(config);
+      });
     });
+
     cy.get(nextButton).click();
     cy.contains('a', 'Account and Transaction API Specification');
     cy.contains(nextButton, 'Run');
--- web/tests/e2e/support/commands.js
+++ web/tests/e2e/support/commands.js
@@ -23,3 +23,45 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+// Loads config fixture JSON and replaces ENV variables with ENV variable values.
+Cypress.Commands.add('configFixture', (file) => {
+  cy.fixture(file).then((template) => {
+    cy.replaceEnvVarConfig(template).then((config) => {
+      const indentedConfig = JSON.stringify(config, null, 2);
+      return indentedConfig;
+    });
+  });
+});
+
+// Replace ENV variables in config template, with ENV variable values.
+//
+// There are several ways to set ENV variables, including:
+//   1. Create a web/cypress.env.json file, e.g.
+//   {
+//     "OZONE_CLIENT_ID": "your_id",
+//     "OZONE_CLIENT_SECRET": "your_secret",
+//     "OZONE_SIGNING_PRIVATE": "-----BEGIN PRIVATE KEY-----\nexample\n-----END PRIVATE KEY-----\n",
+//     "OZONE_SIGNING_PUBLIC": "-----BEGIN CERTIFICATE-----\nexample\n-----END CERTIFICATE-----\n",
+//     "OZONE_TRANSPORT_PRIVATE": "-----BEGIN PRIVATE KEY-----\nexample\n-----END PRIVATE KEY-----\n",
+//     "OZONE_TRANSPORT_PUBLIC": "-----BEGIN CERTIFICATE-----\nexample\n-----END CERTIFICATE-----\n"
+//   }
+//   2. Or export as `CYPRESS_*`
+//
+// For more options see: https://docs.cypress.io/guides/guides/environment-variables.html#Setting
+Cypress.Commands.add('replaceEnvVarConfig', (config) => {
+  const replaceFields = [
+    'signing_private',
+    'signing_public',
+    'transport_private',
+    'transport_public',
+    'client_id',
+    'client_secret',
+  ];
+  replaceFields.forEach((field) => {
+    const envVar = config[field];
+    const value = Cypress.env(envVar);
+    config[field] = value; // eslint-disable-line
+  });
+  return config;
+});
