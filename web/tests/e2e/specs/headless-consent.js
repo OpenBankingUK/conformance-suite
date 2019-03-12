bfe4b3067b44e8344e9b275da0f2a08ab7ddc0b1
robmckinnon <rob@movingflow>
2019-03-12 11:17:36 +0000

    Add custom commands for e2e integration test interactions

--- web/tests/e2e/specs/headless-consent.js
+++ web/tests/e2e/specs/headless-consent.js
@@ -1,37 +1,13 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe('Headless consent model bank test case run', () => {
-  const discoveryTemplate = '#ob-v3-1-ozone-headless';
-  const configJsonTab = '#json-view___BV_tab_button__';
+  const discoveryTemplateId = '#ob-v3-1-ozone-headless';
+  const configTemplate = 'ozone-headless-config.json';
 
-  // Note: We can't use async/await with Cypress then() func, as it does not
-  // return a Promise.
-  // See: https://docs.cypress.io/guides/core-concepts/variables-and-aliases.html#Closures
   it('Gets a PASSED result', () => {
-    cy.visit('https://localhost:8443', { timeout: 8000 });
-    cy.get(discoveryTemplate).click();
-    cy.clickNext();
-    cy.get(configJsonTab).click();
-    cy.configFixture('ozone-headless-config.json').then((config) => {
-      cy.window().then((win) => {
-        const editor = win.ace.edit('configuration-editor');
-        editor.getSession().setValue(config);
-      });
-    });
-
-    cy.clickNext();
-    cy.contains('a', 'Account and Transaction API Specification');
-    cy.nextButtonContains('Run');
-    cy.clickNext();
-    cy.contains('h6', 'PASSED', { timeout: 8000 });
-    cy.nextButtonContains('Next Export', { timeout: 8000 });
-    cy.clickNext();
-    cy.get('#implementer').type('implementer_example');
-    cy.get('#authorised_by').type('authorised_by_example');
-    cy.get('#job_title').type('job_title_example');
-    cy.get('#has_agreed').click({ force: true });
-    cy.nextButtonContains('Export Conformance Report');
-    cy.clickNext();
-    cy.contains('h5', 'Exported Results');
+    cy.selectDiscoveryTemplate(discoveryTemplateId);
+    cy.enterConfiguration(configTemplate);
+    cy.runTestCases();
+    cy.exportConformanceReport();
   });
 });
--- web/tests/e2e/support/commands.js
+++ web/tests/e2e/support/commands.js
@@ -75,3 +75,45 @@ Cypress.Commands.add('clickNext', () => {
 Cypress.Commands.add('nextButtonContains', (text, opts) => {
   cy.contains(nextButton, text, opts);
 });
+
+Cypress.Commands.add('selectDiscoveryTemplate', (templateSelectorId) => {
+  cy.visit('https://localhost:8443', { timeout: 8000 });
+  cy.get(templateSelectorId).click();
+  cy.clickNext();
+});
+
+Cypress.Commands.add('enterConfiguration', (configTemplateFile) => {
+  const configJsonTab = '#json-view___BV_tab_button__';
+  cy.get(configJsonTab).click();
+
+  // Note: We can't use async/await with Cypress then() func, as it does not
+  // return a Promise.
+  // See: https://docs.cypress.io/guides/core-concepts/variables-and-aliases.html#Closures
+  cy.configFixture(configTemplateFile).then((config) => {
+    cy.window().then((win) => {
+      const editor = win.ace.edit('configuration-editor');
+      editor.getSession().setValue(config);
+    });
+  });
+  cy.clickNext();
+  cy.contains('a', 'Account and Transaction API Specification');
+});
+
+Cypress.Commands.add('runTestCases', () => {
+  cy.nextButtonContains('Run');
+  cy.clickNext();
+
+  cy.contains('h6', 'PASSED', { timeout: 8000 });
+  cy.nextButtonContains('Next Export', { timeout: 8000 });
+});
+
+Cypress.Commands.add('exportConformanceReport', () => {
+  cy.clickNext();
+  cy.get('#implementer').type('implementer_example');
+  cy.get('#authorised_by').type('authorised_by_example');
+  cy.get('#job_title').type('job_title_example');
+  cy.get('#has_agreed').click({ force: true });
+  cy.nextButtonContains('Export Conformance Report');
+  cy.clickNext();
+  cy.contains('h5', 'Exported Results');
+});
