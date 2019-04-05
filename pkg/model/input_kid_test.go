85f9e4d504070ee87adc7fceb6738524592f4654
Conor Hackett <chackett1@gmail.com>
2019-04-05 12:00:50 +0100

    go fmt ./...

--- pkg/model/input_kid_test.go
+++ pkg/model/input_kid_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestCalcKid(t *testing.T) {
 	modulus := "tPS6lkBEFf9MuVEfK7BET0oLYe7r6QjQR1SzXqwm37TmcnB8koB66ExmeFizSl8eJuTTjsNCDliGqbGdoe8p_Xw4hRLAPqtEEbq1-sQAAwPUHwgyAABOhIlWBsI6KxYX20UCp5pR4EzqM5cEj_nIvCjw7lmXZaOasMis9utAMw3iKFitduNS5Mj0g523CAes6CnlKusYf--k2l4TpgFRiYFGdVb7T-07xAqlyo5ljLguu8Tz_iwLaqvFKjb_m8gO7dy8P3h8wCv_nbdntxzh17EzsXiMIyh3PNKmxJUmUoAuKkOkpzaRVB5NsjIguIGZrrv0k_hZirxGA_SobsxAvQ"
-	
+
 	kid, err := calcKid(modulus)
 
 	require.NoError(t, err)
