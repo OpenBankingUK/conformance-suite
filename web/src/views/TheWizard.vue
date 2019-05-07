4e40de620d629602756b4600dc236e317848917a
Diego <x@diego.pizza>
2019-05-07 23:17:37 +0100

    move css closer to component

--- web/src/views/TheWizard.vue
+++ web/src/views/TheWizard.vue
@@ -47,4 +47,44 @@ export default {
   min-height: 850px;
   padding-bottom: 0;
 }
+
+.sidebar-arrow {
+  border: solid #fff;
+  border-width: 0 2px 2px 0;
+  display: inline-block;
+  padding: 4px;
+}
+
+.sidebar-arrow.left {
+  transform: translate(11px, 3px) rotate(135deg);
+  -webkit-transform: translate(11px, 3px) rotate(135deg);
+}
+
+.sidebar-arrow.right {
+  transform: translate(9px, 3px) rotate(-45deg);
+  -webkit-transform: translate(9px, 3px) rotate(-45deg);
+}
+
+.collapse-icon {
+  width: 30px;
+  z-index: 1000;
+  height: 30px;
+  background-color: #6180c3;
+  border-radius: 20px;
+  position: absolute;
+  top: 60px;
+  left: 240px;
+}
+
+.collapse-icon:hover {
+  cursor: pointer;
+}
+
+.collapse-icon.collapsed {
+  left: 15px;
+}
+
+.collapsed-sidebar {
+  width: 50px;
+}
 </style>
