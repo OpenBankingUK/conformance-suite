0970ae8218677cd3c22320246dba8d0ebbe4bf92
Diego <x@diego.pizza>
2019-07-22 13:00:53 +0100

    adds requested execution date time to index.js

--- web/src/store/modules/config/index.js
+++ web/src/store/modules/config/index.js
@@ -320,6 +320,7 @@ export const state = {
     currency_of_transfer: 'USD',
     payment_frequency: 'EvryDay',
     first_payment_date_time: '2020-01-01T00:00:00+01:00',
+    requested_execution_date_time: '2020-01-01T00:00:00+01:00',
     acr_values_supported: [],
   },
 
