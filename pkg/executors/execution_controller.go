dcf8a25a435540552391af3eac10c01cdc35055f
Diego <x@diego.pizza>
2019-10-02 16:34:13 +0100

    use defer instead of anon func

--- pkg/executors/execution_controller.go
+++ pkg/executors/execution_controller.go
@@ -93,9 +93,7 @@ func (r *TestCaseRunner) RunTestCases(ctx *model.Context) error {
 // RunConsentAcquisition -
 func (r *TestCaseRunner) RunConsentAcquisition(item TokenConsentIDItem, ctx *model.Context, consentType string, consentIDChannel chan<- TokenConsentIDItem) error {
 	r.runningLock.Lock()
-	defer func() {
-		r.runningLock.Unlock()
-	}()
+	defer r.runningLock.Unlock()
 	if r.running {
 		return errors.New("consent acquisition test cases runner already running")
 	}
