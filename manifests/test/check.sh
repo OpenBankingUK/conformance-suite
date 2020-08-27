74fd293839d10f6d25a0d02f4bcb9efcfeb4affb
Julian Coombes <julian.coombes@googlemail.com>
2020-08-27 16:12:43 +0100

    removed check.sh, unused

--- manifests/test/check.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-FILE="$(pwd)/ob_3.1_accounts_transactions_fca.json"
-
-jq -r '.scripts[].parameters | select(has("x-fapi-interaction-id")) | .["x-fapi-interaction-id"]' "${FILE}" | go run check.go
