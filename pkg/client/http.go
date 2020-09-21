490cda1204dbfce6ddb13531204e973675e2bc44
Julian Coombes <julian.coombes@googlemail.com>
2020-09-21 15:37:45 +0100

    introduce InsecureSkipVerify

--- pkg/client/http.go
+++ pkg/client/http.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"crypto/tls"
 	"net/http"
 	"time"
 )
@@ -11,11 +12,15 @@ const (
 
 // NewHTTPClient returns a more appropriate HTTP client as opposed the default provided by `net/http`
 func NewHTTPClient(timeout time.Duration) *http.Client {
-	return NewHTTPClientWithTransport(timeout, http.DefaultTransport)
+
+	transport := http.DefaultTransport.(*http.Transport).Clone()
+	transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
+	return NewHTTPClientWithTransport(timeout, transport)
 }
 
 // NewHTTPClientWithTransport returns a more appropriate HTTP client as opposed the default provided by `net/http`
-func NewHTTPClientWithTransport(timeout time.Duration, transport http.RoundTripper) *http.Client {
+func NewHTTPClientWithTransport(timeout time.Duration, transport *http.Transport) *http.Client {
 	return &http.Client{
 		Timeout:   timeout,
 		Transport: transport,
