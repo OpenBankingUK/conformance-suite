b7aa53174f07e688883d2ba4b8b48aebb0e03e61
kbendyk <krzysztof.bendyk@accenture.com>
2021-10-27 10:27:26 +0200

    REFAPP-1273 - fix lint errors

--- web/vue.config.js
+++ web/vue.config.js
@@ -21,23 +21,23 @@ module.exports = {
       errors: true,
     },
     proxy: {
-      "/api/*": {
-        target: "https://localhost:8443",
+      '/api/*': {
+        target: 'https://localhost:8443',
         ws: true,
         secure: false,
         debug: true,
         logLevel: 'debug',
         changeOrigin: false,
-        xfwd: true
+        xfwd: true,
       },
-      "/swagger/*": {
-        target: "https://localhost:8443",
+      '/swagger/*': {
+        target: 'https://localhost:8443',
         ws: true,
         secure: false,
         debug: true,
         logLevel: 'debug',
         changeOrigin: false,
-        xfwd: true
+        xfwd: true,
       },
     },
     open: 'Google Chrome',
