8de2a555f58b0cfde679bf10001faa6c0aff6ddb
wkrop <w.kropidlowski@accenture.com>
2024-02-28 16:02:17 +0100

    REFAPP-1314 - remove if else

--- pkg/discovery/tls_validator.go
+++ pkg/discovery/tls_validator.go
@@ -72,17 +72,17 @@ func (v StdTLSValidator) ValidateTLSVersion(uri string) (TLSValidationResult, er
 		if err != nil {
 			return TLSValidationResult{}, errors.Wrapf(err, "unable to parse proxy proxy tls version `%d` to string", proxyState.Version)
 		}
-	} else {
-		state, err := v.getConnectionState(addr)
-		if err != nil {
-			return TLSValidationResult{}, fmt.Errorf("unable to get the connection state for %s", addr)
-		}
+	}
 
-		version = state.Version
-		strVersion, err = tlsVersionToString(state.Version)
-		if err != nil {
-			return TLSValidationResult{}, errors.Wrapf(err, "unable to parse tls version `%d` to string", state.Version)
-		}
+	state, err := v.getConnectionState(addr)
+	if err != nil {
+		return TLSValidationResult{}, fmt.Errorf("unable to get the connection state for %s", addr)
+	}
+
+	version = state.Version
+	strVersion, err = tlsVersionToString(state.Version)
+	if err != nil {
+		return TLSValidationResult{}, errors.Wrapf(err, "unable to parse tls version `%d` to string", state.Version)
 	}
 
 	return TLSValidationResult{
