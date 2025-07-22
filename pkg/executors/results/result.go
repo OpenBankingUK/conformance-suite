9fa36f2c8d10db9541b9eaedde1454c1a94ad714
Christopher Robbertse <chris.robbertse@openbanking.org.uk>
2025-07-22 15:01:24 +0100

    Add Warning option for to TestCases
     - Add Warnings array on Test cases
     - Create new Warning TestCase function
     - Check Failed Test Cases for Warning flag and pass to new Warning TestCase function
     - Log in console if Fail but warning.

--- pkg/executors/results/result.go
+++ pkg/executors/results/result.go
@@ -1,5 +1,7 @@
 package results
 
+import "log"
+
 // TestCase result for a run
 type TestCase struct {
 	Id         string   `json:"id"`
@@ -12,10 +14,23 @@ type TestCase struct {
 	API        string   `json:"-"`
 	APIVersion string   `json:"-"`
 	HttpStatus string   `json:"httpStatusCode"`
+	Warnings   []string `json:"warnings,omitempty"`
 }
 
 // NewTestCaseFail returns a failed test
-func NewTestCaseFail(id string, metrics Metrics, errs []error, endpoint, api, apiVersion, detail, refURI, httpStatus string) TestCase {
+func NewTestCaseFail(id string, metrics Metrics, errs []error, endpoint, api, apiVersion, detail, refURI, httpStatus string, isWarningTest bool) TestCase {
+	// For a warning test, we want to pass the test but include the errors as warnings
+
+	if isWarningTest {
+		warnings := []string{}
+		// Convert errors to warnings and mark as passed
+		for _, err := range errs {
+			warnings = append(warnings, err.Error())
+		}
+
+		log.Printf("Warning test case %s encountered errors: %v", id, warnings)
+		return NewWarningTestCaseResult(id, true, metrics, nil, endpoint, api, apiVersion, detail, refURI, httpStatus, warnings)
+	}
 	return NewTestCaseResult(id, false, metrics, errs, endpoint, api, apiVersion, detail, refURI, httpStatus)
 }
 
@@ -36,6 +51,27 @@ func NewTestCaseResult(id string, pass bool, metrics Metrics, errs []error, endp
 		Detail:     detail,
 		RefURI:     refURI,
 		HttpStatus: httpStatus,
+	}
+}
+
+// NewWarningTestCaseResult return a new TestCase instance
+func NewWarningTestCaseResult(id string, pass bool, metrics Metrics, errs []error, endpoint, apiName, apiVersion, detail, refURI, httpStatus string, warnings []string) TestCase {
+	reasons := []string{}
+	for _, err := range errs {
+		reasons = append(reasons, err.Error())
+	}
+	return TestCase{
+		API:        apiName,
+		APIVersion: apiVersion,
+		Id:         id,
+		Pass:       pass,
+		Metrics:    metrics,
+		Fail:       reasons,
+		Endpoint:   endpoint,
+		Detail:     detail,
+		RefURI:     refURI,
+		HttpStatus: httpStatus,
+		Warnings:   warnings,
 	}
 }
 
